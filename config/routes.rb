a74937dcd0d0ab933b3593ac3174dd0859ca55ae
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-06-16 10:27:19 +0900

    [Add]ログアウト機能の作成#8

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       post "oauth/callback", to: "oauths#callback"
       get "oauth/callback", to: "oauths#callback"
       get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
+      delete 'logout', to: 'user_sessions#destroy'
     end
   end
 end
