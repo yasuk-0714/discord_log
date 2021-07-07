29e83a81634668f6750da2cdaa55d04848ccd759
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-07-07 12:04:38 +0900

    [Add]channel_timesテーブルのリファクタリング

--- app/controllers/api/v1/channel_times_controller.rb
+++ app/controllers/api/v1/channel_times_controller.rb
@@ -3,20 +3,18 @@ class Api::V1::ChannelTimesController < Api::V1::BaseController
 
   def create
     #findは例外が起きるから例外処理をする必要がある
-
-    #アプリを登録しているユーザーかどうかを確認
-    if User.find_by(id: params[:user_id])
-      user_channel = UserChannel.where(user_id: params[:user_id], channel_id: params[:channel_id])
+    if user = User.find_by(id: params[:user_id])
       #UserChannelsテーブルにデータが存在するか確認
-      if user_channel.present?
+      if user_channel = UserChannel.where(user_id: params[:user_id], channel_id: params[:channel_id])
+
         #退出処理
         if params[:state] == 'exit'
-          end_channel = ChannelTime.where(user_channel_id: user_channel[0][:id]).last
-          end_channel.update!(end_time: Time.now)
+          update_channel = user.channel_times.last
+          update_channel.update!(end_time: Time.now)
 
         #入室処理
         elsif params[:state] == 'enter'
-          user_channels = UserChannel.where(user_id: params[:user_id])
+          user_channels = UserChannel.where(user_id: user.id)
           channel_id_list = user_channels.map { |user_channel| user_channel[:id] }
           update_channel = ChannelTime.where(user_channel_id: channel_id_list, end_time: nil)
           #DiscordのカメラON・OFFや音声がON・OFFでもリクエストが飛んでしまうので、以前いたuser_channelと同じパラメーター情報であれば無視する
@@ -24,7 +22,7 @@ class Api::V1::ChannelTimesController < Api::V1::BaseController
 
           #ボイスチャンネルに入室して、退出せずに、違う部屋に入室した場合Botでは判定がつかないので、新しい入室リクエストが来たら、
           #前いたユーザーチャンネルを外部キーに持つchannel_timesテーブルのend_timeに現在時間を入れて、新たな部屋のインスタンスを生成する
-          elsif update_channel.present?
+          elsif update_channel
             update_channel.update(end_time: Time.now)
             start_channel = ChannelTime.new(start_time: Time.now, user_channel_id: user_channel[0][:id])
             start_channel.save!
