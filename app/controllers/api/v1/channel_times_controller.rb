dc956da2b741f0522a847ba03bb350f954463aa1
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-07-07 23:36:43 +0900

    [Fix]channel_timesコントローラーコード修正

--- app/controllers/api/v1/channel_times_controller.rb
+++ app/controllers/api/v1/channel_times_controller.rb
@@ -2,10 +2,8 @@ class Api::V1::ChannelTimesController < Api::V1::BaseController
   protect_from_forgery with: :null_session
 
   def create
-    #findは例外が起きるから例外処理をする必要がある
     if user = User.find_by(id: params[:user_id])
-      #UserChannelsテーブルにデータが存在するか確認
-      if user_channel = UserChannel.where(user_id: params[:user_id], channel_id: params[:channel_id])
+      if user_channel = UserChannel.find_by(user_id: params[:user_id], channel_id: params[:channel_id])
 
         #退出処理
         if params[:state] == 'exit'
@@ -16,20 +14,21 @@ class Api::V1::ChannelTimesController < Api::V1::BaseController
         elsif params[:state] == 'enter'
           user_channels = UserChannel.where(user_id: user.id)
           channel_id_list = user_channels.map { |user_channel| user_channel[:id] }
-          update_channel = ChannelTime.where(user_channel_id: channel_id_list, end_time: nil)
+          update_channel = ChannelTime.find_by(user_channel_id: channel_id_list, end_time: nil)
+
           #DiscordのカメラON・OFFや音声がON・OFFでもリクエストが飛んでしまうので、以前いたuser_channelと同じパラメーター情報であれば無視する
-          if update_channel.present? && update_channel[0][:user_channel_id] == user_channel[0][:id]
+          if update_channel.present? && update_channel.user_channel_id == user_channel.id
 
           #ボイスチャンネルに入室して、退出せずに、違う部屋に入室した場合Botでは判定がつかないので、新しい入室リクエストが来たら、
           #前いたユーザーチャンネルを外部キーに持つchannel_timesテーブルのend_timeに現在時間を入れて、新たな部屋のインスタンスを生成する
           elsif update_channel
             update_channel.update(end_time: Time.now)
-            start_channel = ChannelTime.new(start_time: Time.now, user_channel_id: user_channel[0][:id])
+            start_channel = ChannelTime.new(start_time: Time.now, user_channel_id: user_channel.id)
             start_channel.save!
 
           #新規で入室した場合の処理
           else
-            start_channel = ChannelTime.new(start_time: Time.now, user_channel_id: user_channel[0][:id])
+            start_channel = ChannelTime.new(start_time: Time.now, user_channel_id: user_channel.id)
             start_channel.save!
           end
         end
