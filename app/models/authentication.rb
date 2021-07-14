2fb7da5a63e7a1ef3ff1f7476df70b898b604b80
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-07-14 12:47:13 +0900

    [Fix]トークンを暗号化してdbに保存するよう修正

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,16 +1,14 @@
 class Authentication < ApplicationRecord
-  # before_save :encrypt_access_token
   belongs_to :user
 
   validates :uid, presence: true
   validates :provider, presence: true
 
-  #authenticationsテーブルのトークンを暗号化したら、APIリクエストできないためコメントアウト
-  # def encrypt_access_token
-  #   key_len = ActiveSupport::MessageEncryptor.key_len
-  #   secret = Rails.application.key_generator.generate_key('salt', key_len)
-  #   crypt = ActiveSupport::MessageEncryptor.new(secret)
-  #   self.access_token = crypt.encrypt_and_sign(access_token)
-  #   self.refresh_token = crypt.encrypt_and_sign(refresh_token)
-  # end
+  def encrypt_access_token
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.key_generator.generate_key('salt', key_len)
+    crypt = ActiveSupport::MessageEncryptor.new(secret)
+    self.access_token = crypt.encrypt_and_sign(access_token)
+    self.refresh_token = crypt.encrypt_and_sign(refresh_token)
+  end
 end
