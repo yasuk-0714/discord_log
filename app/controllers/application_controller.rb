02149b5d48497af2a48f55bb2dccbf6b45e5e21c
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-07-15 02:18:46 +0900

    [Add]エラーログが出力されるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,19 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login, only: :mypage
 
+  rescue_from StandardError, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_500(error)
+    logger.error(error.message)
+    logger.error(error.backtrace.join("\n"))
+    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+  end
+
   private
 
   def not_authenticated
