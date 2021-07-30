fb540f7131ef49941edad47f70a3b1c972ff0bdb
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-07-30 17:52:52 +0900

    [Add]サーバー管理画面の実装

---
+++ app/controllers/admin/guilds_controller.rb
@@ -0,0 +1,23 @@
+class Admin::GuildsController < Admin::BaseController
+  before_action :set_guild, only: %i[show destroy]
+
+  def index
+    @guilds = Guild.all
+  end
+
+  def show
+    @channels = @guild.channels
+    @users = @guild.users
+  end
+
+  def destroy
+    @guild.destroy!
+    redirect_to admin_guilds_path, success: 'サーバーを削除しました'
+  end
+
+  private
+
+  def set_guild
+    @guild = Guild.find_by(uuid: params[:uuid])
+  end
+end
