77b5c1abf92ef091b7644c47691c8e6e417a1d57
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-08-09 10:48:26 +0900

    [update]コードリファクタリング

--- app/controllers/api/v1/channel_times_controller.rb
+++ app/controllers/api/v1/channel_times_controller.rb
@@ -11,12 +11,11 @@ class Api::V1::ChannelTimesController < Api::V1::BaseController
 
         #入室処理
         elsif params[:state] == 'enter'
-          channel_id_list = UserChannel.where(user_id: user.id).pluck(:id)
-          update_channel = ChannelTime.find_by(user_channel_id: channel_id_list, end_time: nil)
+          update_channel = user.channel_times.last
 
           #DiscordのカメラON・OFFや音声がON・OFFでもリクエストが飛んでしまうので、以前いたuser_channelと同じパラメーター情報であれば無視する
           if update_channel.present? && update_channel.user_channel_id == user_channel.id
-
+            return
           #ボイスチャンネルに入室して、退出せずに、違う部屋に入室した場合Botでは判定がつかないので、新しい入室リクエストが来たら、
           #前いたユーザーチャンネルを外部キーに持つchannel_timesテーブルを更新して、新たな部屋のインスタンスを生成する
           elsif update_channel
